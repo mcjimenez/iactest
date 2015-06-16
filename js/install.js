2d410ddf55da3447291f32186627b9289ff8030c
Carmen Jimenez Cabezas <cjc@tid.es>
2015-06-16 12:34:02 +0200

    added a bton 4 server

--- js/install.js
+++ js/install.js
@@ -3,7 +3,8 @@
 
   var origin = document.location.origin;
   var _btoInstallClt = document.getElementById('installClt');
-  var _btoInstallSvr = document.getElementById('installSvr');
+  var _btoInstallSvr1 = document.getElementById('installSvr1');
+  var _btoInstallSvr2 = document.getElementById('installSvr2');
   var I = 'iactest';
 
   function install(what) {
@@ -13,14 +14,12 @@
     navigator.mozApps.install(realPath);
   }
 
-  function installSVR() {
-    install('server/server1');
-    install('server/server2');
-  }
-
   window.addEventListener('load', function() {
     _btoInstallClt.addEventListener('click', install.bind(null, 'client'));
-    _btoInstallSvr.addEventListener('click', installSVR);
+    _btoInstallSvr1.addEventListener('click',
+                                     install.bind(null, 'server/server1'));
+    _btoInstallSvr2.addEventListener('click',
+                                     install.bind(null, 'server/server2'));
   });
 
 })(window);
