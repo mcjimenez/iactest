5b7cc7d25a5849e5cc42f4adb34dce8a27c13950
Carmen Jimenez Cabezas <cjc@tid.es>
2015-06-16 15:26:12 +0200

    Added 2 connections at client

--- client/js/iac.js
+++ client/js/iac.js
@@ -1,12 +1,21 @@
 (function(imports) {
 'use strict';
-  var WHERE = 'testiac';
-  var RULES = {
+  var WHERE_SVR1 = 'iacsvr1';
+  var WHERE_SVR2 = 'iacsvr2';
+  var RULES_SVR1 = {
     "minimumAccessLevel": "web",
     "pageURLs": [
       "^https://.*\\.github\\.io/iactest.*"
     ]
   };
+  var RULES_SVR2 = {
+    "minimumAccessLevel": "web",
+    "pageURLs": [
+      "^https://.*\\.github\\.io/iactest.*"
+    ]
+  };
+
+  var app = null;
 
   var _eventsEntries = document.getElementById('eventsEntries');
   var _btoPing = document.getElementById('ping');
@@ -26,36 +35,41 @@
     where.appendChild(li);
   }
 
-  function connect() {
+  function init() {
     navigator.mozApps.getSelf().onsuccess = function(evt) {
-      var app = evt.target.result;
-      if (app.connect) {
-        app.connect(WHERE, RULES).then(function onConnAccepted(ports) {
-          var i = 1;
-          ports.forEach(port => {
-            addTxt('CJC - IAC connection success. Adding listener!', whatEntry);
-
-            port.onmessage = function(evt) {
-              addTxt('Received:' +
-                  (evt.data ? JSON.stringify(evt.data) : 'No data'), whatEntry);
-            };
-            msg.num = _count++;
-            addTxt('Sending:' + JSON.stringify(msg), whatEntry);
-            port.postMessage(msg);
-
-            (i--) && (_port = port);
-          });
-        }, function onConnRejected(reason) {
-          addTxt('Cannot connect:' + reason, whatEntry);
-        });
-      }
+      app = evt.target.result;
     };
+  }
 
+  function connect(where, rules) {
+    if (app.connect) {
+      app.connect(where, rules).then(function onConnAccepted(ports) {
+        var i = 1;
+        ports.forEach(port => {
+          addTxt('CJC - IAC connection success. Adding listener!', whatEntry);
+          port.onmessage = function(evt) {
+            addTxt('Received:' +
+                  (evt.data ? JSON.stringify(evt.data) : 'No data'), whatEntry);
+          };
+          msg.num = _count++;
+          addTxt('Sending:' + JSON.stringify(msg), whatEntry);
+          port.postMessage(msg);
+
+          (i--) && (_port = port);
+        });
+      }, function onConnRejected(reason) {
+        addTxt('Cannot connect:' + reason, whatEntry);
+      });
+    };
   }
 
   window.addEventListener('load', function() {
-    connect();
-
+    //connect();
+    init();
+    console.log('Connectiong ' + WHERE_SVR1);
+    connect(WHERE_SVR1, RULES_SVR1);
+    console.log('Connectiong ' + WHERE_SVR2);
+    connect(WHERE_SVR2, RULES_SVR2);
     _btoPing.addEventListener('click', function send() {
       msg.num = _count++;
       addTxt('Sending:' + JSON.stringify(msg), whatEntry);
